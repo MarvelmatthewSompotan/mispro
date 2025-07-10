6b99910a9f9df3dbbd5beae893bd5decef3c3fb1
jery koupun <s22129077@student.unklab.ac.id>
2025-07-10 10:42:15 +0800

    Update Table Transportations

--- backend/database/migrations/2025_07_09_141152_create_transportations_table.php
+++ backend/database/migrations/2025_07_09_141152_create_transportations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transportations', function (Blueprint $table) {
             $table->id('transport_id');
             $table->enum('type', ['OWN CAR', 'SCHOOL BUS']);
-            $table->text('pickup_point')->nullable();
+            $table->enum('pickup_point', ['BITUNG 1', 'BITUNG 2', 'BITUNG 3', 'BITUNG 4', 'BITUNG 5','KEMA / MINAWEROT', 'UNKLAB / AIRMADIDI', 'TATELU / MATUNGKAS','MALL 1', 'MALL 2', 'MALL 3', 'MALL 4 / CITRALAND','STAR FOTO 1', 'STAR FOTO 2', 'TELING KAROMBASAN','MANDIRI 1', 'MANDIRI 2', 'MANDIRI 3','MGP 1', 'MGP 2', 'MGP 3', 'MGP 4', 'MGP 5 / TUMINTING', 'MGP 6','KUWIL / PEMDA', 'ASABRI / MAUMBI', 'DISTRICT M', 'TUMINTING','GRANDMAX 1503', 'GRANDMAX 1498', 'GRANDMAX 1407','VARSITY BITUNG', 'VARSITY MANADO', 'VARSITY KEMA'])->nullable();
             $table->enum('policy_signed', ['SIGNED', 'NOT SIGNED']);
         });
     }
