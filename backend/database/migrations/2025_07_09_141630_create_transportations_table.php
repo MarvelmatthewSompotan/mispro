b1bd6f9cb7db44aebd8c1af6a3b11ef2d11c23bf
jery koupun <s22129077@student.unklab.ac.id>
2025-07-15 10:19:48 +0800

    Update Fields

--- backend/database/migrations/2025_07_09_141630_create_transportations_table.php
+++ backend/database/migrations/2025_07_09_141630_create_transportations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('transportations', function (Blueprint $table) {
             $table->id('transport_id');
-            $table->enum('type', ['Own Car', 'School Bus']);
+            $table->enum('type', ['Own car', 'School bus']);
             $table->unsignedBigInteger('pickup_point_id')->nullable();
             $table->enum('Transportation policy', ['Signed', 'Not Signed']);
             $table->foreign('pickup_point_id')->references('pickup_point_id')->on('pickup_points')->onDelete('set null');
