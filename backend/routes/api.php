6ae926829703474bdde69335cf445130d40d7718
Deeva-Civia <s22210557@student.unklab.ac.id>
2025-07-16 22:22:53 +0800

    feat(api): add routes for sections, programs, majors, transportations, and residences

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\RegistrationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MasterDataController;
+use App\Http\Controllers\RegistrationController;
 
 Route::post('/registration', [RegistrationController::class, 'store']);
 
@@ -12,4 +13,11 @@ Route::get('/students/{id}', [RegistrationController::class, 'show']);
 
 Route::get('/students/{id}/history', [RegistrationController::class, 'history']);
 
+Route::prefix('master')->group(function () {
+  Route::get('/sections', [MasterDataController::class, 'getSections']);
+  Route::get('/programs', [MasterDataController::class, 'getPrograms']);
+  Route::get('/majors', [MasterDataController::class, 'getMajors']);
+  Route::get('/transportations', [MasterDataController::class, 'getTransportations']);
+  Route::get('/residences', [MasterDataController::class, 'getResidenceHalls']);
+})
 ?>
