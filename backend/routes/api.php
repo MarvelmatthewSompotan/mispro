ea683f0027115947146f609048cf4aa354fd66b7
Deeva-Civia <s22210557@student.unklab.ac.id>
2025-07-18 12:02:41 +0800

    feat(api): add registration routes (start, context, clear, submit)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,13 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MasterDataController;
 use App\Http\Controllers\RegistrationController;
 
-Route::post('/registration', [RegistrationController::class, 'store']);
+Route::post('/registration/start', [RegistrationController::class, 'startRegistration']);
+Route::get('/registration/context', [RegistrationController::class, 'getRegistrationContext']);
+Route::delete('/registration/context', [RegistrationController::class, 'clearRegistrationContext']);
+Route::post('/registration/submit', [RegistrationController::class, 'store']);
 
-Route::get('/students', [RegistrationController::class, 'index']);
+// Route::get('/students', [RegistrationController::class, 'index']);
 
-Route::get('/students/{id}', [RegistrationController::class, 'show']);
+// Route::get('/students/{id}', [RegistrationController::class, 'show']);
 
-Route::get('/students/{id}/history', [RegistrationController::class, 'history']);
+// Route::get('/students/{id}/history', [RegistrationController::class, 'history']);
 
 Route::get('/registration-option', [MasterDataController::class, 'getRegistrationOption']);
 ?>
