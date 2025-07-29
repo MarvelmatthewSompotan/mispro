31d69467ee3b1068dc039afcb61c790bcfebd42a
Deeva-Civia <s22210557@student.unklab.ac.id>
2025-07-29 17:20:20 +0800

    feat(controller:master-data): add pickup point and discount type retrieval

--- backend/app/Http/Controllers/MasterDataController.php
+++ backend/app/Http/Controllers/MasterDataController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Major;
 use App\Models\Program;
 use App\Models\Section;
+use App\Models\PickupPoint;
 use App\Models\SchoolClass;
+use App\Models\DiscountType;
 use Illuminate\Http\Request;
 use App\Models\ResidenceHall;
 use App\Models\Transportation;
@@ -27,6 +29,8 @@ class MasterDataController extends Controller
             ];
         });
         
+        $pickupPoints = PickupPoint::select('pickup_point_id', 'name')->get();
+        
         return response()->json([
             'sections' => $sections,
             'majors' => $majors,
@@ -34,6 +38,7 @@ class MasterDataController extends Controller
             'programs' => Program::all(),
             'transportations' => Transportation::select('transport_id', 'type')->get(),
             'residence_halls' => ResidenceHall::all(),
+            'dicount_types' => DiscountType::select('discount_type_id', 'name')->get(),
             'student_status' => ['New', 'Old', 'Transferee'],
             'academic_status' => ['REGULAR', 'SIT-IN', 'OTHER'],
             'gender' => ['MALE', 'FEMALE'],
@@ -42,32 +47,7 @@ class MasterDataController extends Controller
             'payment_type' => ['Tuition Fee', 'Residence Hall'],
             'payment_method' => ['Full Payment', 'Installment'],
             'financial_policy_contract' => ['Signed', 'Not Signed'],
-            'pickup_points' => [
-                'Bitung 1: Bus #10 - Pusat Kota',
-                'Bitung 2: Bus #11 - Pelabuhan',
-                'Bitung 3: Bus #12 - Wangurer',
-                'Bitung 4: Bus #13 - Girian',
-                'Bitung 5: Bus #23 - Girian',
-                'Bitung 6: Bus #14 - Pusat Kota',
-                'Kema/Minawerot: Bus #15 - Kema 3',
-                'Airmadidi/Agape/Unklab',
-                'Tatelu: Bus #37 Pertigaan Pinily Tatelu',
-                'Maumbi: Walanda Maramis - Maumbi',
-                'Pemda/Kawangkoan/Asabri/Rizky',
-                'Sunflower/Perkamil: SF Paal Dua - Martadinata',
-                'MGP/Lapangan: MGP 1 - Bus #27 - Tamansari',
-                'MGP/Lapangan: MGP 2 - Bus #28 - Adipura',
-                'MGP/Lapangan: MGP 3 - Bus #26 - Lippo Plaza',
-                'Tuminting/Kombos: Bus #25 & #38 - Gereja Torsina',
-                'Bank Mandiri Sudirman (Manado): Bus #19 & #20',
-                'Winangun/Teling/Karombasan: RS Advent Teling',
-                'Citraland/Kantor Pemasaran Citraland',
-                'Bank Niaga 1: Bus #21 - Starphoto',
-                'Bank Niaga 2: Bus #22 - Starphoto',
-                'Malalayang 1: Bus #30 - Lapangan Bantik',
-                'Malalayang 2: Bus #31 - Lapangan Bantik',
-                'Malalayang 3: Bus #32 - Lapangan Bantik',
-            ],
+            'pickup_points' => $pickupPoints,
         ]);
     }
 }
