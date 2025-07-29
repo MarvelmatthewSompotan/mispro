6cd583489e872fbebef3167f84afa1cb05574a87
Deeva-Civia <s22210557@student.unklab.ac.id>
2025-07-29 17:05:41 +0800

    add pickup points and discount types to MasterDataSeeder

--- backend/database/seeders/MasterDataSeeder.php
+++ backend/database/seeders/MasterDataSeeder.php
@@ -7,7 +7,9 @@ use App\Models\Program;
 use App\Models\Section;
 use App\Models\Semester;
 use App\Models\SchoolYear;
+use App\Models\PickupPoint;
 use App\Models\SchoolClass;
+use App\Models\DiscountType;
 use App\Models\ResidenceHall;
 use App\Models\Transportation;
 use Illuminate\Database\Seeder;
@@ -45,6 +47,17 @@ class MasterDataSeeder extends Seeder
             ResidenceHall::create(['type' => $type]);
         }
 
+        $discountTypes = [
+            'Beasiswa',
+            'Special Diskon',
+            'Staff',
+            'Weiver',
+            'IP'
+        ];
+        foreach ($discountTypes as $discount) {
+            DiscountType::firstOrCreate(['name' => $discount]);
+        }
+
         // semester
         $semesters = [
             ['name' => 'One', 'number' => '1'],
@@ -61,9 +74,41 @@ class MasterDataSeeder extends Seeder
             SchoolYear::create(['year' => $year]);
         }
 
+        // pickup point
+        $pickupPoints = [
+            'Bitung 1: Bus #10 - Pusat Kota',
+            'Bitung 2: Bus #11 - Pelabuhan',
+            'Bitung 3: Bus #12 - Wangurer',
+            'Bitung 4: Bus #13 - Girian',
+            'Bitung 5: Bus #23 - Girian',
+            'Bitung 6: Bus #14 - Pusat Kota',
+            'Kema/Minawerot: Bus #15 - Kema 3',
+            'Airmadidi/Agape/Unklab',
+            'Tatelu: Bus #37 Pertigaan Pinily Tatelu',
+            'Maumbi: Walanda Maramis - Maumbi',
+            'Pemda/Kawangkoan/Asabri/Rizky',
+            'Sunflower/Perkamil: SF Paal Dua - Martadinata',
+            'MGP/Lapangan: MGP 1 - Bus #27 - Tamansari',
+            'MGP/Lapangan: MGP 2 - Bus #28 - Adipura',
+            'MGP/Lapangan: MGP 3 - Bus #26 - Lippo Plaza',
+            'Tuminting/Kombos: Bus #25 & #38 - Gereja Torsina',
+            'Bank Mandiri Sudirman (Manado): Bus #19 & #20',
+            'Winangun/Teling/Karombasan: RS Advent Teling',
+            'Citraland/Kantor Pemasaran Citraland',
+            'Bank Niaga 1: Bus #21 - Starphoto',
+            'Bank Niaga 2: Bus #22 - Starphoto',
+            'Malalayang 1: Bus #30 - Lapangan Bantik',
+            'Malalayang 2: Bus #31 - Lapangan Bantik',
+            'Malalayang 3: Bus #32 - Lapangan Bantik',
+        ];
+
+        foreach($pickupPoints as $name) {
+            PickupPoint::create(['name' => $name]);
+        }
+        
+        // grade
         $grades = ['N', 'K1', 'K2', '1','2','3','4','5','6','7','8','9','10','11','12'];
         foreach ($grades as $grade) {
-            // Konversi angka string jadi integer (jika bisa)
             $gradeInt = is_numeric($grade) ? (int)$grade : null;
 
             if (in_array($grade, ['N', 'K1', 'K2'])) {
