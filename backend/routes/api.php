753824993132e7ea6eeb0737e7f381b005d8a9d3
Deeva-Civia <s22210557@student.unklab.ac.id>
2025-07-29 17:02:05 +0800

    Added registration routes to web.php and api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,12 +6,6 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\MasterDataController;
 use App\Http\Controllers\RegistrationController;
 
-Route::post('/registration/start', [RegistrationController::class, 'startRegistration']);
-Route::get('/registration/context', [RegistrationController::class, 'getRegistrationContext']);
-Route::delete('/registration/context', [RegistrationController::class, 'clearRegistrationContext']);
-Route::post('/registration/submit', [RegistrationController::class, 'store']);
-Route::get('/registration/preview/{applicationId}', [RegistrationController::class, 'showPreview']);
-
 // Route::get('/students', [RegistrationController::class, 'index']);
 
 // Route::get('/students/{id}', [RegistrationController::class, 'show']);
@@ -19,6 +13,9 @@ Route::get('/registration/preview/{applicationId}', [RegistrationController::cla
 // Route::get('/students/{id}/history', [RegistrationController::class, 'history']);
 
 Route::get('/registration-option', [MasterDataController::class, 'getRegistrationOption']);
-Route::get('/students/search', [StudentController::class, 'searchStudents']);
-Route::get('/students/{student_id}/latest-application', [StudentController::class, 'getLatestApplication']);
+Route::prefix('students')->group(function () {
+  Route::get('/search', [StudentController::class, 'searchStudents']);
+  Route::get('/{student_id}/latest-application', [StudentController::class, 'getLatestApplication']);
+});
+
 ?>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('registration')->group(function () {
+    Route::post('/start', [RegistrationController::class, 'startRegistration']);
+    Route::get('/context', [RegistrationController::class, 'getRegistrationContext']);
+    Route::delete('/context', [RegistrationController::class, 'clearRegistrationContext']);
+    Route::post('/submit', [RegistrationController::class, 'store']);
+    Route::get('/preview/{applicationId}', [RegistrationController::class, 'showPreview']);
+});
+
+Route::get('/csrf-token', function () {
+    $token = csrf_token();
+    return response()->json([
+        'csrf_token' => $token,
+    ])->withCookie(cookie('XSRF-TOKEN', $token, 120));
 });
