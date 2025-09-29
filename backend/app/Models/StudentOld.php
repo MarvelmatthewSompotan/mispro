bf300a0e6cd990e490a772896f91649fe75ef556
jerykoupun <s22129077@student.unklab.ac.id>
2025-09-29 12:18:59 +0800

    Adding Migration and Model Student_Old

---
+++ backend/app/Models/StudentOld.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentOld extends Model
+{
+    protected $table = 'student_old_table';
+    
+    protected $primaryKey = 'studentold_id';
+    public $incrementing = false;
+    protected $keyType = 'int';
+    
+    protected $fillable = [
+        'studentold_id',
+        'nisn',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'nickname',
+        'place_of_birth',
+        'date_of_birth',
+        'gender',
+        'student_address',
+        'student_phone',
+        'student_email',
+        'previous_school',
+        'religion',
+        'nik',
+        'section',
+        'grade',
+        'father_name',
+        'father_occupation',
+        'father_company',
+        'father_address',
+        'father_phone',
+        'father_email',
+        'mother_name',
+        'mother_occupation',
+        'mother_company',
+        'mother_address',
+        'mother_phone',
+        'mother_email',
+        'guardian_name',
+        'relation_to_student',
+        'guardian_address',
+        'guardian_phone',
+    ];
+
+}
--- backend/database/migrations/2025_07_09_131651_create_students_table.php
+++ backend/database/migrations/2025_07_09_131651_create_students_table.php
@@ -34,7 +34,7 @@ return new class extends Migration
             $table->string('religion', 30);
             $table->string('academic_status_other', 50)->nullable();
             $table->enum('enrollment_status', ['ACTIVE','INACTIVE',]);
-            $table->enum('inactive_status', ['GRADUATE','TRANSFEREE','EXPELLED']);
+            $table->string('inactive_status', 50)->nullable();
             $table->enum('student_status', ['New','Old','Transferee']);
             $table->enum('academic_status', ['REGULAR','SIT-IN','OTHER']);
             $table->timestamp('registration_date')->useCurrent();
