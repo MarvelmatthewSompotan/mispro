4cc2f20196714d3a4c420106f8a4c661074ce914
HizkiaPalar <Hizkiapalar294@gmail.com>
2025-10-14 11:29:10 +0800

    Adding components Radio button and fix icon filter and sort

--- frontend/src/components/atoms/FilterButton.js
+++ frontend/src/components/atoms/FilterButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Controls.css";
+import filterIcon from "../../assets/filter.png";
 
 const FilterButton = ({ disabled = false, onClick, title = "Filter" }) => {
   return (
@@ -11,21 +12,7 @@ const FilterButton = ({ disabled = false, onClick, title = "Filter" }) => {
       title={title}
       disabled={disabled} // Tambahkan atribut disabled
     >
-      <svg
-        width="18"
-        height="18"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M3 4h18l-7 8v6l-4 2v-8L3 4z"
-          stroke="currentColor"
-          strokeWidth="2"
-          fill="none"
-          strokeLinejoin="round"
-        />
-      </svg>
+      <img src={filterIcon} alt="Filter Icon" width="24" height="24" />
     </button>
   );
 };
--- frontend/src/components/atoms/SortButton.js
+++ frontend/src/components/atoms/SortButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Controls.css";
+import sortIcon from "../../assets/sort.png";
 
 const SortButton = ({ disabled = false, direction = "none", onClick, title = "Sort" }) => {
   return (
@@ -11,10 +12,12 @@ const SortButton = ({ disabled = false, direction = "none", onClick, title = "So
       title={title}
       disabled={disabled} // Tambahkan atribut disabled
     >
-      <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M8 7l4-4 4 4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" opacity={direction === "down" ? 0.25 : 1}/>
-        <path d="M16 17l-4 4-4-4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" opacity={direction === "up" ? 0.25 : 1}/>
-      </svg>
+      <img
+        src={sortIcon}
+        alt="Sort Icon"
+        width="24"
+        height="24"
+      />
     </button>
   );
 };
