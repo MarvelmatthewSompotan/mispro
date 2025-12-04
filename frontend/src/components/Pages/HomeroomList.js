2dfa82a24e8bbd010a23ba2900798286d2194f80
Deeva-Civia <s22210557@student.unklab.ac.id>
2025-12-04 15:43:37 +0800

    update folder name

---
+++ frontend/src/components/Pages/HomeroomList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomeroomList = () => (
+  <div style={{ padding: 32 }}>
+    <h2>Homeroom List</h2>
+    <p>Test</p>
+  </div>
+);
+
+export default HomeroomList;
